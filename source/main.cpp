394d646b6d059ce9feba3552bdcf2b6a2d5bee19
Matvey <sumenkovmatvey.inc@gmail.com>
2023-06-17 02:48:06 +0300

    Delete XLM Parser and add debug.json

--- source/main.cpp
+++ source/main.cpp
@@ -8,25 +8,21 @@ int main(int argc, char *argv[]) {
     signal(SIGTERM, signalHandler); // обработка стандартного завершения работы программы
     signal(SIGINT, signalHandler);
 
-    std::string config_filename("config/debug.xml");
+    std::string config_filename("config/debug.json");
 
-    settings Settings;
-    Settings.load(config_filename);
-
-    /**
     int port = 8001;
     int logs = 1;
-    fint port = 8001;
-    int logs = 1;
+//    fint port = 8001;
+//    int logs = 1;
     for(int i=1;i<argc;++i){
         if(std::string(argv[i])=="-p"){
             i++;
-            port = atoi(argv[i]);
+            port = std::atoi(argv[i]);
             continue;
         }
         if(std::string(argv[i])=="-l"){
             i++;
-            logs = atoi(argv[i]);
+            logs = std::atoi(argv[i]);
             continue;
         }
     }
@@ -34,7 +30,7 @@ int main(int argc, char *argv[]) {
 
     switch (logs) {
         case 0:
-            init_deep_debug_log;
+            init_deep_debug_log();
             break;
         case 1:
             init_debug_log();
@@ -51,56 +47,10 @@ int main(int argc, char *argv[]) {
 
     BOOST_LOG_TRIVIAL(info)<<"Server started at port: "<<port<<" logs type: "<<logs;
 
-
     boost::thread_group threads;
-
     threads.create_thread(boost::bind( Server::accept_thread, port));
-
     threads.create_thread(Server::handle_clients_thread);
-
     threads.join_all();
-    or(int i=1;i<argc;++i){
-        if(std::string(argv[i])=="-p"){
-            i++;
-            port = atoi(argv[i]);
-            continue;
-        }
-        if(std::string(argv[i])=="-l"){
-            i++;
-            logs = atoi(argv[i]);
-            continue;
-        }
-    }
-
-
-    switch (logs) {
-        case 0:
-            init_deep_debug_log;
-            break;
-        case 1:
-            init_debug_log();
-            break;
-        case 2:
-            init_production_log();
-            break;
-        default:
-            std::cout<<"Неправильные параметры запуска\n";
-            return 1;
-    }
-
-    logging::add_common_attributes();
-
-    BOOST_LOG_TRIVIAL(info)<<"Server started at port: "<<port<<" logs type: "<<logs;
-
-
-    boost::thread_group threads;
-
-    threads.create_thread(boost::bind( Server::accept_thread, port));
-
-    threads.create_thread(Server::handle_clients_thread);
-
-    threads.join_all();
-    */
 
     return 0;
 }
